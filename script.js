6f7df61f10b4f55264421923894e1d754e6f54f8
smilewendy7 <smilewendy7@gmail.com>
2021-05-28 22:23:44 -0400

    version 1 Complete looks like it work, but not perfect

--- script.js
+++ script.js
@@ -3,15 +3,25 @@
 const url = "http://localhost:3000/notes"
 const form = document.querySelector('#note-form')
 const noteList = document.querySelector('#note-list')
+const noteText = document.getElementById('note-text').value
 
 // add event listener to submit 
 form.addEventListener('submit', event => {
+
     event.preventDefault()
     const noteText = document.getElementById('note-text').value
-    console.log(noteText)
+    //edit
+    if ( document.querySelector('#save-note').innerText === "edit"){
+        //refresh
+        location.reload()
+    }
+
+    //*** 
+    
+    else {console.log("here is my note",  noteText)
     createNote(noteText)
-    //!!!!!!!!!
-    // updateNote(noteText)
+    }
+    
 })
 
 
@@ -21,21 +31,29 @@ noteList.addEventListener("click", event=> {
     // when user click delete run deleteNote()
     if(event.target.classList.contains('delete')) {
         deleteNote(event.target)
-        console.log(event.target.classList)
+        // console.log("this is target", event.target)
     }
 })
 
 
-//!!!!!!!!!!!edit a note from the dom ******>>>>
-//click --> edit input --> save ---> update  
+    //!!!!!!!!!!!edit a note from the dom ******>>>>
+        //click --> edit input --> save ---> update  
+// click link the id of note to edit ... 
 
 noteList.addEventListener("click", event=> {
     // when user click edit run edit note()
     if(event.target.classList.contains('edit')) {
+        // updateNote(event.target)
+        document.querySelector('#save-note').innerText = "edit"
+        // link the ID in dom with note ID .....**** noteID**** ??
+        // point to note # 
+        console.log("this is target", event.target)
         updateNote(event.target)
     }
 })
 
+
+// list notes ... ????
 function listNotes(){
     //get request 
     fetch(url)
@@ -80,42 +98,60 @@ function deleteNote(element){
 
 //Update #####################!!!!!!!!!!!!*************************
 
-// function updateNote(element) {
-//     // noteText update 
-//     // then save 
-//     const noteText = document.getElementById('note-text').value
-//     const noteId = element.parentElement.id
-//     fetch(url + "/" + `${noteId}`, {
-//         method: 'PATCH',
-//         headers: {'Content-Type': 'application/json'},
-//         body: JSON.stringify({
-//             title: noteText,
-//             body: noteText,
-//             //using moment
-//             create_at: moment().format()
-//         })
-//     })
-//     .then(response => response.json())
-//     .then(data => renderNoteItem(data))
+// Edit then update.... 
+    // first remove ,then add... 
+
+function updateNote(element){
+    const noteText = document.getElementById('note-text').value
+    const noteId = element.parentElement.id
+    fetch(url + "/" + `${noteId}`, {
+        // ** -- replace old note content with new input 
+        //** -- save the new input to DOM and Json, show on the page
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            //replace old note to new inputs ... 
+            title: noteText,
+            body: noteText,
+            //using moment
+            create_at: moment().format()
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("I want this", data)
+        // then click on edit, save changes to dom  
+        //upate(data)
+        data=> element.parentElement.replace()
+        
+            })
+}
+
+// function update(content) {
+
+
 // }
 
 
-// rendering 
-function renderNoteItem(noteObj){
-    const itemEl = document.createElement('li')
-    itemEl.id = noteObj.id
 
-    renderNoteText(itemEl, noteObj)
-    console.log(itemEl)
+// rendering each note.. 
+function renderNoteItem(eachNote){
+    const itemEl = document.createElement('li')
+    itemEl.id = eachNote.id
+    renderNoteText(itemEl, eachNote)
     noteList.appendChild(itemEl)
 }
 
-function renderNoteText(noteListItem, noteObj) {
+// what is noteListItem is the whole row ... 
+function renderNoteText(noteListItem, eachNote) {
     
-    noteListItem.innerHTML = `<span class="dib w-60">${noteObj.body}</span><i class="ml2 dark-red fas fa-times delete"></i><i class="ml3 fas fa-edit edit"></i>`
+    noteListItem.innerHTML = `<span class="dib w-60">${eachNote.body}</span><i class="ml2 dark-red fas fa-times delete"></i><i class="ml3 fas fa-edit edit"></i>`
 }
 
 listNotes();
+
+
+// if it is editing update input ,
 
 
 
